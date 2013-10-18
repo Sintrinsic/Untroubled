b77c432c7d767d50308d2af469846a938d3f7093
jgeorge <jgeorge@localhost>
2013-10-18 10:40:09 -0500

    Adding portscanner

---
+++ networking/portScanner.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 18, 2013
+
+@author: jgeorge
+'''
+
+class portScanner(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
